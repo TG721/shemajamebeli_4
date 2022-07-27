af3d944eb3bb62773691d69e3c0f4556525861c9
Tengiz <Tengiz152@gmail.com>
2022-07-27 10:58:55 +0400

    commit number 2

--- app/src/main/java/com/example/tbc_shemajamebeli_4/ConfigFragment.kt
+++ app/src/main/java/com/example/tbc_shemajamebeli_4/ConfigFragment.kt
@@ -1,59 +1,52 @@
 package com.example.tbc_shemajamebeli_4
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.tbc_shemajamebeli_4.databinding.FragmentConfigBinding
+import com.example.tbc_shemajamebeli_4.databinding.FragmentGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ConfigFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConfigFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var binding: FragmentConfigBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_config, container, false)
+        binding = FragmentConfigBinding.inflate(inflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.type3x3?.setOnClickListener{
+            val action = ConfigFragmentDirections.navigateToGameFragment("3x3")
+            Navigation.findNavController(view).navigate(action)
+        }
+        binding?.type4x4?.setOnClickListener{
+            val action = ConfigFragmentDirections.navigateToGameFragment("4x4")
+            Navigation.findNavController(view).navigate(action)
+        }
+        binding?.type5x5?.setOnClickListener{
+            val action = ConfigFragmentDirections.navigateToGameFragment("5x5")
+            Navigation.findNavController(view).navigate(action)
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConfigFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConfigFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = ConfigFragment()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
--- app/src/main/java/com/example/tbc_shemajamebeli_4/MainActivity.kt
+++ app/src/main/java/com/example/tbc_shemajamebeli_4/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tbc_shemajamebeli_4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+val items = mutableListOf<Int>()
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
